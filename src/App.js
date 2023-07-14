5216b5ee36be4a2a6fafda5056f5774b23f446ee
vigneshnm64 <vigneshnm64@gmail.com>
2023-07-14 15:21:33 +0530

    change text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Welcome vignesh to react web page 
+          Welcome vignesh to react web page to serve and build
         </a>
       </header>
     </div>
