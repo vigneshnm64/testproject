29e39532d84634962cf0fc51b8b18eb8bb04881d
vigneshnm64 <vigneshnm64@gmail.com>
2023-07-14 14:23:55 +0530

    message checked

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Welcome vignesh
+          Welcome vignesh to react
         </a>
       </header>
     </div>
